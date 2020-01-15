f329012131253292aa8366bfda46629605a8cbd4
rafiudd <sirin6867@gmail.com>
2020-01-15 13:47:13 +0700

    add helper

---
+++ helpers/jwt.js
@@ -0,0 +1,28 @@
+const expressJwt = require('express-jwt');
+const config = require('config.json');
+const userService = require('../modules/users/users.service');
+
+module.exports = jwt;
+
+function jwt() {
+    const secret = config.secret;
+    return expressJwt({ secret, isRevoked }).unless({
+        path: [
+            // public routes that don't require authentication
+            '/',
+            '/api/users/login',
+            '/api/users/register'
+        ]
+    });
+}
+
+async function isRevoked(req, payload, done) {
+    const user = await userService.getById(payload.sub);
+
+    // revoke token if user no longer exists
+    if (!user) {
+        return done(null, true);
+    }
+
+    done();
+};
