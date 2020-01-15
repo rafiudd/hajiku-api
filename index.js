ea8cfeac450af1e7008475d5ff73d08e5155f27b
rafiudd <sirin6867@gmail.com>
2020-01-15 13:51:46 +0700

    change port

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use('/api/users', require('./modules/users/users.controller'));
 app.use(errorHandler);
 
 // start server
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 8080;
 const server = app.listen(port, function () {
     console.log('Server listening on port ' + port);
 });
