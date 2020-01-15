9cf8b970edd656c510bade67917311e70a000ad9
rafiudd <sirin6867@gmail.com>
2020-01-15 13:47:01 +0700

    change index structure

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
-let express = require('express');
-let bodyParser = require('body-parser');
-let mongoose = require('mongoose');
-let app = express();
+require('rootpath')();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const jwt = require('helpers/jwt');
+const errorHandler = require('helpers/error-handler');
 
-let apiRoutes = require("/home/rafiudd/Documents/quizqoeh/quizquh-apis/routes/router.js");
-
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
-mongoose.connect('mongodb://rafiudd:123123123@0.0.0.0:27017/quizquh', { useNewUrlParser: true});
-const db = mongoose.connection;
+// use JWT auth to secure the api
+app.use(jwt());
 
-if(!db)
-    console.log("Error connecting db")
-else
-    console.log("Db connected successfully")
-
-const port = process.env.PORT || 8080;
-
-app.get('/', (req, res) => res.send('Hello World with Express'));
-app.use('/api', apiRoutes);
+// api routes
 
 
-app.listen(port, function () {
-    console.log("Running RestHub on port " + port);
+app.get('/', function (req, res) {
+    res.json({
+        status: 'API Its Working',
+        message: 'Welcome to RESTHub crafted with love!'
+    });
+});
+app.use('/api/users', require('./modules/users/users.controller'));
+
+// global error handler
+app.use(errorHandler);
+
+// start server
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+const server = app.listen(port, function () {
+    console.log('Server listening on port ' + port);
 });
