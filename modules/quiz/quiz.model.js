6cd26923e85234082749a639be63dc7729b7cebc
rafiudd <sirin6867@gmail.com>
2020-01-24 14:22:39 +0700

    add module quiz

---
+++ modules/quiz/quiz.model.js
@@ -0,0 +1,23 @@
+﻿const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+    username: { type: String },
+    id : { type : Number },
+    title : { type : String },
+    imageLink : { type  : String },
+    videoLink : { type : String },
+    description : { type : String },
+    subMateri : [
+        {
+            id : { type : Number },
+            title : { type : String },
+            description : { type : String }
+        }
+    ],
+    isHome : { type : Boolean }
+});
+
+schema.set('toJSON', { virtuals: true });
+
+module.exports = mongoose.model('Haji', schema);
