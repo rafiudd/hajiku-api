56ae860f6ee5534f88bdb56c2529c615c5056796
rafiudd <sirin6867@gmail.com>
2020-02-04 18:07:08 +0700

    remove unused code

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,12 +1,11 @@
 const expressJwt = require('express-jwt');
 const config = require('config.json');
-const userService = require('../modules/users/users.service');
 
 module.exports = jwt;
 
 function jwt() {
     const secret = config.secret;
-    return expressJwt({ secret, isRevoked }).unless({
+    return expressJwt({ secret }).unless({
         path: [
             // public routes that don't require authentication
             '/',
@@ -15,14 +14,3 @@ function jwt() {
         ]
     });
 }
-
-async function isRevoked(req, payload, done) {
-    const user = await userService.getById(payload.sub);
-
-    // revoke token if user no longer exists
-    if (!user) {
-        return done(null, true);
-    }
-
-    done();
-};
