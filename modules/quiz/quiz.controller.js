566de7f5e9963e93aa42ddf61508762fcea64fa9
rafiudd <sirin6867@gmail.com>
2020-02-04 17:35:02 +0700

    add api check user answer and get result

--- modules/quiz/quiz.controller.js
+++ modules/quiz/quiz.controller.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../../helpers/db');
+const jwt = require('jsonwebtoken');
+
 const Quiz = db.Quiz;
+const User = db.User;
+const ResultQuiz = db.ResultQuiz;
 let dataSoal = require('../../data/soal.json')
 // routes
 
 router.post('/post/all', create);
 router.get('/all', getAll);
+router.get('/result', getResultQuiz);
 router.get('/', getById);
 router.delete('/delete', _delete);
+router.post('/post/quiz', checkAnswer);
 
 module.exports = router;
 
@@ -40,5 +46,82 @@ async function getById(req, res, next) {
 async function _delete(req, res, next) {
     let query = await Quiz.remove();
     let result = res.json({"message" : "Success Remove Haji" , "code" : 204, "data" : query})
+    return result
+}
+
+async function checkAnswer(req,res) {
+    let model = {
+        _id : req.query.id,
+    }
+
+    let getQuiz = await Quiz.findById(model._id);
+    let payloadAnswer = req.body.answer;
+    let answer = [];
+
+    getQuiz.question.map( obj => {
+        answer.push(obj.answer);
+    })
+
+    let isTrue = 0;
+    let isFalse = 0;
+
+    for(let i=0; i<answer.length; i++) {
+        console.log(payloadAnswer[i],['gsfgfs'],answer[i])
+        if(payloadAnswer[i] === answer[i]) {
+            isTrue ++
+        } else {
+            isFalse --
+        }
+    }
+    let token = req.headers.authorization.replace('Bearer ','');
+    
+    let decode = jwt.decode(token);
+    let userId = decode.sub
+
+    let query = await User.findById(userId);
+    // console.log(query,['QIERY'])
+    
+    function formatDate(date) {
+        let d = new Date(date);
+        let formatedDate, hours, minutes, milisecond, formatedTime;
+      
+        formatedDate = d.getFullYear() + "-" + ("0" + d.getMonth() + 1).slice(-2) + "-" + ("0" + d.getDate()).slice(-2);
+        hours = ("0" + d.getHours()).slice(-2);
+        minutes = ("0" + d.getMinutes()).slice(-2);
+        milisecond = ("0" + d.getSeconds()).slice(-2);
+      
+        formatedTime = hours + ":" + minutes + ":" + milisecond
+        return formatedDate + " " + formatedTime  
+    }
+  
+      
+    let resultAnswer = {
+        "userId" : userId,
+        "fullname" : query.fullname,
+        "email" : query.email,
+        "userAnswer" : payloadAnswer,
+        "trueAnswer" : isTrue,
+        "falseAnswer" : Math.abs(isFalse),
+        "value" : isTrue * 10,
+        "timestamp" : formatDate(new Date())
+    }
+
+    await ResultQuiz.insertMany(resultAnswer)
+
+    let result = res.json(
+        {
+            "message" : "Success Cleared Quiz" ,
+            "code" : 204, 
+            "data" : resultAnswer
+        }
+    )
+    
+    return result
+}
+
+async function getResultQuiz(req, res, next) {
+    let query = await ResultQuiz.find();
+    let result = res.json({"message" : "Success Get All Soal" , "code" : 200, "data" : query })
+        
     return result
 }
